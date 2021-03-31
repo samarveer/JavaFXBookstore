bd5238535809491a82a6053940535a36505a7fcd
Samarveer Sandhu <Samarveer Sandhu@192.168.0.202>
2021-03-31 16:06:37 -0400

    This is the COE528 Final Project.

---
+++ src/bookstore/Bookstore.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bookstore;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Samarveer Sandhu
+ */
+public class Bookstore extends Application {
+    
+    @Override
+    public void start(Stage window) {
+        window.setTitle("Login Window");
+
+        Label label1 = new Label("Username");
+        Label label2 = new Label("Password");
+        
+        
+        window.setTitle("Hello World!");
+        window.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
